e14ff936fd235f13bd996c93c8e140c43609a3dc
Alex Spencer <alexander.j.spencer@gmail.com>
2019-09-23 16:32:56 -0700

    all route handler functions work

--- run-model.js
+++ run-model.js
@@ -1,11 +1,22 @@
-// require('dotenv').config();
-// const connect = require('./lib/connect');
-// // const mongoose = require('mongoose');
+require('dotenv').config();
+const connect = require('./lib/connect');
+const mongoose = require('mongoose');
 
-// connect();
+connect();
 
-// const Movie = require('./lib/models/movie');
+const Movie = require('./lib/models/movie');
 
-// Movie.create({
-
-// });
+Movie.create({
+  name: 'Titanic',
+  rating: 'PG-13',
+  yearReleased: 1997,
+  locations: {
+    secondaryLocations: 'The Ocean'
+  }
+})
+  .then(createdMovie => {
+    console.log(createdMovie);
+  })
+  .then(() => {
+    mongoose.disconnect();
+  });
