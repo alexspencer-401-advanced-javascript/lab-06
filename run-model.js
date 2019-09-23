e78817192092446429c17401a94efd9764e9f712
Alex Spencer <alexander.j.spencer@gmail.com>
2019-09-23 14:14:26 -0700

    passed test re: valid model for all movie object properties

---
+++ run-model.js
@@ -0,0 +1,11 @@
+// require('dotenv').config();
+// const connect = require('./lib/connect');
+// // const mongoose = require('mongoose');
+
+// connect();
+
+// const Movie = require('./lib/models/movie');
+
+// Movie.create({
+
+// });
